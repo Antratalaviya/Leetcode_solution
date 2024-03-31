f50cf875dd9d04a5bb612b7c027b1adf91876458
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-03-31 14:47:45 +0530

    Time: 48 ms (79.76%) | Memory: 49 MB (52.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -5,31 +5,31 @@
  */
 
 var search = function (nums, target) {
-    return nums.indexOf(target);
+    // return nums.indexOf(target);
 
-    // let l = 0;
-    // let r = nums.length - 1;
+    let l = 0;
+    let r = nums.length - 1;
 
-    // while (l <= r) {
-    //     let mid = Math.floor((l + r) / 2);
-    //     if (nums[mid] === target) {
-    //         return mid;
-    //     }
-    //     if (nums[l] <= nums[mid]) {
-    //         if (nums[l] <= target && target < nums[mid]) {
-    //             r = mid - 1;
-    //         } else {
-    //             l = mid + 1;
-    //         }
-    //     } else {
-    //         if (nums[mid] < target && target <= nums[r]) {
-    //             l = mid + 1;
-    //         }
-    //         else {
-    //             r = mid - 1;
-    //         }
-    //     }
-    // }
-    // return -1;
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[l] <= nums[mid]) {
+            if (nums[l] <= target && target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[r]) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+    }
+    return -1;
 };
 
