161c12f357ce5b1d960f8a5b65752fab11d7a35a
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-04-01 11:13:51 +0530

    Time: 44 ms (89.86%) | Memory: 48.4 MB (91.14%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,16 +3,20 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    s = s.trim();
-    let last = "";
-    let c = s.length - 1;
-    while (c > 0 && s[c] !== ' ') {
-        c--;
-    }
-    for (let i = c; i < s.length; i++) {
-        if (s[i] !== ' ') {
-            last = last + s[i];
-        }
-    }
-    return last.length;
+    s = s.trim().split(" ");
+    return s[s.length - 1].trim().length;
 };
+
+// 47ms and 48.12mb
+ // s = s.trim();
+    // let last = "";
+    // let c = s.length - 1;
+    // while (c > 0 && s[c] !== ' ') {
+    //     c--;
+    // }
+    // for (let i = c; i < s.length; i++) {
+    //     if (s[i] !== ' ') {
+    //         last = last + s[i];
+    //     }
+    // }
+    // return last.length;
