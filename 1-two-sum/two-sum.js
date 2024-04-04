56120519fa07cfc7942f8485eb36d149f1dab97e
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-04-04 18:59:55 +0530

    Time: 67 ms (53.31%) | Memory: 51.9 MB (12.51%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let hashMap = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        hashMap.set(nums[i], i)
-    }
+    let hashMap = new Map();
+
     for (let i = 0; i < nums.length; i++) {
         let complement = target - nums[i]
-        if (hashMap.has(complement) && hashMap.get(complement)!==i) {
+        if (hashMap.has(complement) && hashMap.get(complement) !== i) {
             return [i, hashMap.get(complement)]
+        } else {
+            hashMap.set(nums[i], i);
         }
     }
 };
