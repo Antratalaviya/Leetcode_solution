615d9c88254aea8e3590208749b859909579ccb4
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-04-04 19:43:21 +0530

    Time: 112 ms (42.67%) | Memory: 56.4 MB (19.84%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -10,23 +10,29 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
-    let dummyHead = new ListNode(0)
-    let current = dummyHead
-    let carry = 0
-    while (l1 || l2) {
-        let x = l1 ? l1.val : 0
-        let y = l2 ? l2.val : 0
-        let sum = x + y + carry
-        carry = Math.floor(sum/10)
-        current.next = new ListNode(sum%10)
-        current = current.next
+var addTwoNumbers = function (l1, l2, carry = 0) {
+    if (!l1 && !l2 && !carry) return null;
+    let total = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    carry = parseInt(total / 10);
+    return new ListNode(total % 10, addTwoNumbers(l1?.next, l2?.next, carry));
 
-        if(l1) l1=l1.next
-        if(l2) l2= l2.next
-    }
-    if(carry>0){
-        current.next= new ListNode(carry)
-    }
-    return dummyHead.next
 };
+
+    // let dummyHead = new ListNode(0)
+    // let current = dummyHead
+    // let carry = 0
+    // while (l1 || l2) {
+    //     let x = l1 ? l1.val : 0
+    //     let y = l2 ? l2.val : 0
+    //     let sum = x + y + carry
+    //     carry = Math.floor(sum/10)
+    //     current.next = new ListNode(sum%10)
+    //     current = current.next
+
+    //     if(l1) l1=l1.next
+    //     if(l2) l2= l2.next
+    // }
+    // if(carry>0){
+    //     current.next= new ListNode(carry)
+    // }
+    // return dummyHead.next
