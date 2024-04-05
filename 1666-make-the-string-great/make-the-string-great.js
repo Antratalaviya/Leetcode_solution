d75d0c2b0b01e53567f2f2b76532c7b2cf4abc8a
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-04-05 23:56:27 +0530

    Time: 63 ms (37.16%) | Memory: 50.5 MB (84.91%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.js
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -3,19 +3,29 @@
  * @return {string}
  */
 
-// sliding window
 var makeGood = function (s) {
-    let i = 0;
-    while (i < s.length - 1) {
-        if ((s[i] === s[i].toUpperCase() && s[i + 1] === s[i].toLowerCase()) ||
-            (s[i + 1] === s[i + 1].toUpperCase() && s[i] === s[i + 1].toLowerCase())) {
-            s = s.slice(0, i) + s.slice(i + 2);
-            if (i > 0) {
-                i--;
-            }
+    let stack = [];
+    for (let char of s) {
+        let peek = stack.length ? stack[stack.length - 1] : null;
+        if ((char.toUpperCase() === peek || char.toLowerCase() === peek) && char !== peek) {
+            stack.pop();
             continue;
         }
-        i++;
+        stack.push(char);
     }
-    return s;
+    return stack.join('');
 };
+
+    // let i = 0;
+    // while (i < s.length - 1) {
+    //     if ((s[i] === s[i].toUpperCase() && s[i + 1] === s[i].toLowerCase()) ||
+    //         (s[i + 1] === s[i + 1].toUpperCase() && s[i] === s[i + 1].toLowerCase())) {
+    //         s = s.slice(0, i) + s.slice(i + 2);
+    //         if (i > 0) {
+    //             i--;
+    //         }
+    //         continue;
+    //     }
+    //     i++;
+    // }
+    // return s;
