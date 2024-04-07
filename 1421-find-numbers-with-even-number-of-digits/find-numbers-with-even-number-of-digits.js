a923d81747a94e3a5efceaa8a23d0d09a2a5218c
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-04-07 14:05:21 +0530

    Time: 45 ms (94.75%) | Memory: 50.8 MB (20.18%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -5,19 +5,18 @@
 var findNumbers = function (nums) {
     let cnt = 0;
     for (let num of nums) {
-        let n = num.toString();
-        if (n.length % 2 === 0) {
+        if (countDigit(num) % 2 === 0) {
             cnt++
         }
     }
     return cnt;
 };
 
-// function countDigit(num) {
-//     let sum = 0;
-//     while (num > 0) {
-//         num = Math.floor(num / 10);
-//         sum++;
-//     }
-//     return sum
-// }
+function countDigit(num) {
+    let sum = 0;
+    while (num > 0) {
+        num = Math.floor(num / 10);
+        sum++;
+    }
+    return sum
+}
