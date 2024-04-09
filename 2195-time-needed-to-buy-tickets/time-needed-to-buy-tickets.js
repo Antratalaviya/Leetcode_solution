a31cab923048dd15fd07f89d52c0dab4f2e16d58
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-04-09 16:55:26 +0530

    Time: 50 ms (76.50%) | Memory: 49.2 MB (39.89%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function (tickets, k) {
+    let time = 0;
+    let i = 0;
+    while (i < tickets.length && tickets[k] !== 0) {
+        if (tickets[i]) {
+            tickets[i] -= 1;
+            time++;
+        }
+        i++;
+        if (i === tickets.length) i = 0;
+    }
+    return time;
+};
