a77626f230d4683ae415f8a54219469e57ca2f61
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-04-09 17:24:59 +0530

    Time: 45 ms (93.38%) | Memory: 50.4 MB (50.15%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.js
+++ 22-generate-parentheses/generate-parentheses.js
@@ -5,25 +5,41 @@
 var generateParenthesis = function (n) {
     let result = [];
 
-    function backtrack(list, close, open) {
-        if (close === open && close === n) {
-            result.push([...list].join(''));
+    function backtrack(list, open, close) {
+        if (list.length === 2 * n) {
+            result.push(list);
         }
         if (open < n) {
-            list.push('(');
-            open++;
-            backtrack(list, close, open)
-            open--;
-            list.pop();
+            backtrack(list + "(", open + 1, close);
         }
-        if (close < open && close < n) {
-            list.push(")");
-            close++;
-            backtrack(list, close, open);
-            close--;
-            list.pop();
+        if (close < open) {
+            backtrack(list + ")", open, close + 1);
         }
     }
-    backtrack([], 0, 0);
-    return result;
+    backtrack('', 0, 0);
+    return result
 };
+
+ // let result = [];
+
+    // function backtrack(list, close, open) {
+    //     if (close === open && close === n) {
+    //         result.push([...list].join(''));
+    //     }
+    //     if (open < n) {
+    //         list.push('(');
+    //         open++;
+    //         backtrack(list, close, open)
+    //         open--;
+    //         list.pop();
+    //     }
+    //     if (close < open && close < n) {
+    //         list.push(")");
+    //         close++;
+    //         backtrack(list, close, open);
+    //         close--;
+    //         list.pop();
+    //     }
+    // }
+    // backtrack([], 0, 0);
+    // return result;
