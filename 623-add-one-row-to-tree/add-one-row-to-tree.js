6d58224cc0e165fba6f4a711457a6db3f993298a
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-04-16 11:06:44 +0530

    Time: 80 ms (35.53%) | Memory: 54.9 MB (77.63%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @param {number} depth
+ * @return {TreeNode}
+ */
+var addOneRow = function (root, val, depth) {
+    if (!root) return;
+    if(depth===1){
+        return new TreeNode(val, root, null); 
+    }
+    function bfs(node, v, d) {
+        if (d === depth) return;
+        if (d + 1 === depth) {
+            let newNodeL = new TreeNode(v, node.left, null);
+            let newNodeR = new TreeNode(v, null, node.right);
+            node.left = newNodeL;
+            node.right = newNodeR;
+        }
+        if (node.left) bfs(node.left, v, d + 1);
+        if (node.right) bfs(node.right, v, d + 1);
+    }
+
+    bfs(root, val, 1);
+    return root;
+};
