c438e8472e92a885c3fb7a53125c4074d6539dff
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-07-03 11:59:45 +0530

    Time: 2 ms (98.36%) | Memory: 56.9 MB (70.23%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,51 +1,23 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas = 0, totalCost = 0, n = gas.length;
+        int n = gas.length;
+
+        int sum = 0;
+        int index = 0;
+        int total = 0;
 
         for (int i = 0; i < n; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                total += sum;
+                index = i + 1;
+                sum = 0;
+            }
         }
-
-        if (totalGas < totalCost)
+        total += sum;
+        if (total < 0) {
             return -1;
-
-        int arr[][] = new int[n][3];
-
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = gas[i];
-            arr[i][1] = cost[i];
-            arr[i][2] = i;
         }
-
-        Arrays.sort(arr, (a, b) -> {
-            if (a[0] - a[1] == b[0] - b[1]) {
-                return a[1] - b[1];
-            } else {
-                return (b[0] - b[1]) - (a[0] - a[1]);
-            }
-        });
-
-      
-
-        for (int row[] : arr) {
-            if (canMove(row[2], gas, cost) == true) {
-                return row[2];
-            }
-        }
-        return -1;
-    }
-
-    public boolean canMove(int i, int[] gas, int[] cost) {
-        int maxIndex = i, fuel = 0, n = gas.length;
-        do {
-            fuel += gas[i];
-            if (fuel < cost[i]) {
-                return false;
-            }
-            fuel -= cost[i];
-            i = (i + 1) % n;
-        } while (i != maxIndex);
-        return true;
+        return index;
     }
 }
