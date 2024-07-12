12fd91ce35ca847e04793d0868eebce4ae517680
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2024-07-12 16:48:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (22.24%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,27 +14,29 @@
  * }
  */
 class Solution {
-    public TreeNode createBT(int[] postorder, HashMap<Integer, Integer> map, int inStart, int inEnd, int poStart,
-            int poEnd) {
-        if (inStart > inEnd || poStart > poEnd) {
+    int i = 0, p = 0;
+
+    public TreeNode createBT(int[] postorder, int[] inorder, int stop) {
+        if (p < 0) {
             return null;
         }
-        TreeNode root = new TreeNode(postorder[poEnd]);
-        int mid = map.get(root.val);
-        int numLeft = mid - inStart;
 
-        root.left = createBT(postorder, map, inStart, mid - 1, poStart, poStart + numLeft - 1);
-        root.right = createBT(postorder, map, mid + 1, inEnd, poStart + numLeft, poEnd - 1);
+        if (inorder[i] == stop) {
+            i--;
+            return null;
+        }
+
+        TreeNode root = new TreeNode(postorder[p--]);
+
+        root.right = createBT(postorder, inorder, root.val);
+        root.left = createBT(postorder, inorder, stop);
 
         return root;
     }
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
-        }
-        return createBT(postorder, map, 0, inorder.length - 1, 0, postorder.length - 1);
+        p = postorder.length - 1;
+        i = p;
+        return createBT(postorder, inorder, Integer.MIN_VALUE);
     }
 }
