138fd7c9303e6752f698a56b17b0b70d23b2d1a7
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-06-06 18:18:25 +0530

    Time: 86 ms (5.03%) | Memory: 64.4 MB (6.88%) - LeetSync

---
+++ 72-edit-distance/edit-distance.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+    const m = word1?.length;
+    const n = word2?.length;
+    let prev = new Array(n + 1).fill(0);
+    let curr = new Array(n + 1).fill(0);
+
+    console.log(m);
+    console.log(n);
+
+    for (let i = 1; i <= n; i++) {
+        prev[i] = i
+    }
+
+    for (let i = 1; i <= m; i++) {
+        curr[0] = i;
+        for (let j = 1; j <= n; j++) {
+            if (word1[i - 1] === word2[j - 1]) {
+                curr[j] = prev[j - 1];
+            } else {
+                curr[j] = Math.min(prev[j - 1], prev[j], curr[j - 1]) + 1
+            }
+        }
+        prev = curr;
+        curr = new Array(n + 1).fill(0);
+    }
+
+    console.log(prev)
+    console.log(curr)
+
+    return prev[n];
+};
