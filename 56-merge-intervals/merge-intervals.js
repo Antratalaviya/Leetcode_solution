608a41a5e4021de941dea43cf0fe8a537b5e896a
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-06-21 18:03:17 +0530

    Time: 8 ms (67.41%) | Memory: 65.3 MB (40.64%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+    let res = [];
+    intervals = intervals?.sort((a, b) => a[0] - b[0]);
+
+    for (let interval of intervals) {
+        if (!res?.length || res[res?.length - 1][1] < interval[0]) {
+            res.push(interval);
+        } else {
+            const end = Math.max(interval[1], res[res?.length - 1][1]);
+            res[res?.length - 1][1] = end;
+        }
+    }
+
+    return res;
+};
