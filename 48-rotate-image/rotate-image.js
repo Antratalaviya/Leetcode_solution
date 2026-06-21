8e932f3586ee4b2b43a2aa91d80f75f173859066
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-06-21 19:06:09 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (28.38%) - LeetSync

---
+++ 48-rotate-image/rotate-image.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+    let m = matrix?.length;
+    let n = matrix[0]?.length;
+    for (let i = 0; i < m; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n / 2; j++) {
+            [matrix[i][j], matrix[i][m - j - 1]] = [matrix[i][m - j - 1], matrix[i][j]];
+        }
+    }
+};
