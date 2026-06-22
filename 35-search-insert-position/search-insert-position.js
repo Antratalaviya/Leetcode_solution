10097586172058da6b83a9f6dc50927ae0f073f3
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-06-22 20:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (71.83%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -3,20 +3,21 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function (nums, target) {
-    let l = 0;
-    let r = nums.length - 1;
 
-    while (l <= r) {
-        let mid = Math.floor((l + r) / 2);
-        if (nums[mid] === target) {
-            return mid;
-        }
-        if (target < nums[mid]) {
-            r = mid - 1;
-        } else {
-            l = mid + 1;
+var searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums?.length - 1;
+    let index = right + 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] >= target) {
+            index = mid;
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
         }
     }
-    return l;
+
+    return index;
 };
