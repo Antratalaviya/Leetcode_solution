3c0e1fea4700e86d21fb1cba988a75d533caca75
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-06-24 20:20:01 +0530

    Time: 15 ms (17.76%) | Memory: 61.6 MB (5.36%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+const countHours = (piles, k) => {
+    let totalHours = 0;
+    for (let pile of piles) {
+        totalHours += Math.ceil(pile / k)
+    }
+    return totalHours;
+}
+var minEatingSpeed = function (piles, h) {
+    let max = piles[0];
+
+    for (let pile of piles) {
+        max = Math.max(max, pile);
+    }
+
+    let low = 1;
+    let high = max;
+    let ans = max;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        let hour = countHours(piles, mid);
+
+        if (hour <= h) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+};
