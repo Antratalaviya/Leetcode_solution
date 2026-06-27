cf22cb738144c0cafd67d9bd76cfdcedd2d22aca
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-06-27 20:35:14 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (98.11%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+    let low = 0;
+    let high = nums?.length - 1;
+
+    while (low < high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] === nums[high]) {
+            high--;
+            continue;
+        }
+        if (nums[mid] <= nums[high]) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return nums[low];
+};
