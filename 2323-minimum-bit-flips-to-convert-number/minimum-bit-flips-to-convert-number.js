4da38781200883aa6858e7bbed005eecc2d63c55
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-07-08 18:13:39 +0530

    Time: 1 ms (24.14%) | Memory: 53 MB (93.10%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.js
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.js
@@ -8,7 +8,8 @@ var minBitFlips = function (start, goal) {
     let count = 0;
 
     while (setBits) {
-        count += setBits & 1;;
+        //clear right most LSB
+        count += setBits & 1;
         setBits >>= 1;
     }
     return count;
