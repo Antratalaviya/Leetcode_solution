0920984e70a5d61339ddc9a02c1ede4dd8b14371
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-07-19 14:20:39 +0530

    Time: 17 ms (58.00%) | Memory: 71.2 MB (30.81%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
@@ -2,50 +2,19 @@
  * @param {number[]} heights
  * @return {number}
  */
-const findNSE = (arr) => {
-    let n = arr?.length;
-    let st = [];
-    let ans = new Array(n).fill(n - 1);
-
-
-    return ans;
-}
-const findPSEE = (arr) => {
-    let n = arr?.length;
-
-    return ans;
-}
 var largestRectangleArea = function (heights) {
+    heights.push(0);
     let n = heights?.length;
-    const leftSmall = new Array(n);
-    const rightSmall = new Array(n);
-
+    let maxArea = 0;
     let st = [];
 
-    //Left small
     for (let i = 0; i < n; i++) {
-        while (st?.length && heights[st[st?.length - 1]] >= heights[i]) {
-            st.pop();
+        while (st?.length && heights[st[st?.length - 1]] > heights[i]) {
+            const height = heights[st.pop()];
+            let width = st?.length ? i - st[st?.length - 1] - 1 : i;
+            maxArea = Math.max(maxArea, (height * width));
         }
-        leftSmall[i] = st?.length ? st[st?.length - 1] + 1 : 0;
         st.push(i);
-    }
-
-    st = [];
-
-    //Right small
-    for (let i = n - 1; i >= 0; i--) {
-        while (st?.length && heights[st[st?.length - 1]] >= heights[i]) {
-            st.pop();
-        }
-        rightSmall[i] = st?.length ? st[st?.length - 1] - 1 : n - 1;
-        st.push(i);
-    }
-
-    let maxArea = 0;
-    for (let i = 0; i < n; i++) {
-        let width = rightSmall[i] - leftSmall[i] + 1;
-        maxArea = Math.max(maxArea, (heights[i] * width));
     }
     return maxArea;
 };
