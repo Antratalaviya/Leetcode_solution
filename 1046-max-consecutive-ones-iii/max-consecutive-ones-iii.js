385fb87ec74dead5f7d7d7d33614dff9c426bb07
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-07-25 19:25:46 +0530

    Time: 2 ms (85.00%) | Memory: 62.3 MB (64.65%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function (nums, k) {
+    let l = 0, zeroCount = 0, max = 0;
+    for (let r = 0; r < nums.length; r++) {
+        if (nums[r] === 0) {
+            zeroCount++;
+        }
+        while (zeroCount > k) {
+            if (nums[l] === 0) {
+                zeroCount--;
+            }
+            l++
+        }
+        max = Math.max(max, r - l + 1);
+    }
+    return max;
+};
+
+/**
+Initialize two pointer l, r == 0;
+zeroCunt = 0 variable to store no of zero encountered in window.
+interate through array and expand the right boundary of window.
+if zeroCount exceed the k then shrink window by moving left until zeroCount becomes valid.
+calculate length of window at each step. 
+ */
