5b07f2bc399a436364f10690d07a31dc29947a02
Antra Talaviya <116263197+Antratalaviya@users.noreply.github.com>
2026-07-27 20:05:10 +0530

    Time: 19 ms (56.80%) | Memory: 56.9 MB (46.76%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {
+    let maxFreq = 0, maxLen = 0, l = 0;
+    let freq = {};
+
+    for (let r = 0; r < s?.length; r++) {
+        freq[s[r]] = (freq[s[r]] || 0) + 1;
+
+        maxFreq = Math.max(maxFreq, freq[s[r]]);
+
+        while ((r - l + 1) - maxFreq > k) {
+            freq[s[l]]--;
+            l++
+        }
+        maxLen = Math.max(maxLen, r - l + 1);
+    }
+    return maxLen;
+};
